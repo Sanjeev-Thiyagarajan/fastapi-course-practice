6cd31753051a7656eaf15b5e8b177da324fd43e5
Sanjeev Thiyagarajan <sanjeevkt720@gmail.com>
2021-06-08 01:07:58 -0400

    lesson 9

--- main.py
+++ main.py
@@ -72,8 +72,23 @@ async def delete_posts(id: int):
 
     index = find_index_post(id)
     print(index)
-    if not index:
+    if index == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+async def update_post(id: int, post: Post):
+    index = find_index_post(id)
+    print(index)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exist")
+
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+
+    return {"data": post_dict}
