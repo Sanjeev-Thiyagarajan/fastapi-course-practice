276a3f760703d7240f6a05c41b45cf830459511e
Sanjeev Thiyagarajan <sanjeevkt720@gmail.com>
2021-06-07 23:09:14 -0400

    lesson 5

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from random import randrange
 
 from pydantic import BaseModel
 
+my_posts = [{"id": 1, "title": "my first post", "content": "random text"},
+            {"id": 2, "title": "my second post", "content": "I like dogs"}]
+
 
 class Post(BaseModel):
     title: str
@@ -17,16 +20,18 @@ app = FastAPI()
 
 @ app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "hello there"}
 
 
 @ app.get("/posts")
 async def get_posts():
-    return {"data": "getting posts"}
+    return {"data": my_posts}
 
 
 @ app.post("/posts")
 async def create_post(post: Post):
     post_dict = post.dict()
-    # print(post.title)
+    post_dict['id'] = randrange(0, 1000)
+    my_posts.append(post_dict)
+    print(my_posts)
     return {"data": post}
